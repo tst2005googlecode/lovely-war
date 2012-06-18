223899f6d79b6fa2aa97d8e642253436910264fe
jdourlens <jdourlens@076bb3cb-2966-6a85-65df-23be00364c35>
2012-06-18 15:48:09 +0000

    add Love2d Callbacks and middleclass lib

--- trunk/main.lua
+++ trunk/main.lua
@@ -1,1 +1,20 @@
 
+require('externallibs/middleclass/middleclass')
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
+
+function love.mousepressed(x, y, button)
+end
+
+function love.keypressed(k)
+end
+
+function love.quit()
+end
