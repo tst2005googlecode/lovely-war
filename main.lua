13fff338bbf8a25fbb8bc7d311bb608187e45f61
jdourlens <jdourlens@076bb3cb-2966-6a85-65df-23be00364c35>
2012-06-19 08:03:44 +0000

    Peon draw

--- main.lua
+++ main.lua
@@ -3,13 +3,27 @@ require('externallibs/middleclass/middleclass')
 require('objects')
 
 function love.load()
-
+   p1 = peon:new(2, 2, 1) 
 end
 
 function love.update(dt)
+
 end
 
 function love.draw()
+
+local i = 1
+while i < MAP_X do
+   local j = 1
+   while j < MAP_Y do
+      if map[i][j] ~= 0 then
+	 map[i][j]:draw()
+      end
+      j = j + 1
+   end
+   i = i + 1
+end
+
 end
 
 function love.mousepressed(x, y, button)
--- objects.lua
+++ objects.lua
@@ -1,5 +1,23 @@
 objects = {}
+
+MAP_X = 125
+MAP_Y = 125
+
+TILE_X = 32
+TILE_Y = 32
+
 map = {}
+
+local i = 1
+while i < MAP_X do
+   map[i] = {}
+   local j = 1
+   while j < MAP_Y do
+      map[i][j] = 0
+      j = j + 1
+   end
+   i = i + 1
+end
 
 require('obj/baseObj')
 require('obj/peon')
